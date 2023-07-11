28b8d28396cbb150e31e98917ba7511f91368cd9
Dalia Lizbeth Nahol <dalia.nahol@gmail.com>
2023-07-11 06:48:18 -0700

    morning 9 min easy LC pom

---
+++ practice-2023/leetcode/1071.GreatestCommonDivisorOfStrings.js
@@ -0,0 +1,25 @@
+Attempt 1: 9 min.
+
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+
+// Brute Force
+/*
+0. Iterate str1 and str2
+1. find largest substring match, largediv
+2. check if that match is a divisor of 
+A) str1
+B) str 2
+C) if so, return largediv
+else. largediv = largediv minus last char
+D) repeat #2 until largediv is a divisor
+
+
+ */
+
+var gcdOfStrings = function(str1, str2) {
+    
+};
