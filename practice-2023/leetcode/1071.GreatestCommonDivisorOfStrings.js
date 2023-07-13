257b7056a03b04b971b2fdde5d33d6308dd61d4e
Dalia Lizbeth Nahol <dalia.nahol@gmail.com>
2023-07-13 09:10:52 -0700

    attempt 4 speed run, shortest solution :), 1071 gcd strings

--- practice-2023/leetcode/1071.GreatestCommonDivisorOfStrings.js
+++ practice-2023/leetcode/1071.GreatestCommonDivisorOfStrings.js
@@ -1,6 +1,25 @@
 7.13.2023 Thurs
 Speed Practice Solution:
-20min 
+25min 
+
+Attempt 4:
+5min
+
+solved 2x
+Shortest solution
+
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    return str1+str2 === str2+str1?
+    str1.substring(0,gcd(str1.length,str2.length)):
+    ""
+};
+
+var gcd = (a,b) => b===0? a: gcd(b, a%b);
 
 
 Attempt 3:
